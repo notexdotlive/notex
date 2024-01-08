33742abd1267d8adffb992756810a58f4d08c815
gelzinn <60621494+gelzinn@users.noreply.github.com>
2024-01-08 11:26:23 -0300

    styles: add new sizes and background animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,27 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'hd': '1280px',
+        hd: '1280px',
         'full-hd': '1920px',
+      },
+      backgroundSize: {
+        '100%': '100% auto',
+        '200%': '200% auto',
+        '300%': '300% auto',
+        '400%': '400% auto',
+      },
+      animation: {
+        'background-linear': 'background-animate 6s ease infinite',
+      },
+      keyframes: {
+        'background-animate': {
+          'from, to': {
+            'background-position': '0 0',
+          },
+          '50%': {
+            'background-position': '100% 0',
+          },
+        },
       },
     },
   },
