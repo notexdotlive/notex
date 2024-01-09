42d18d9bc63723e0061601c9559e2e2847b00f10
gelzinn <60621494+gelzinn@users.noreply.github.com>
2024-01-09 13:29:08 -0300

    feat: refactor homepage layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const Homepage = () => {
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between gap-4 w-full p-0 sm:p-8 md:p-0 -mb-64 z-10">
+        <div className="flex flex-col items-start justify-start gap-4 w-full h-full p-0 sm:p-8 md:p-0 -mb-64 z-10">
           <div className="flex flex-col items-center md:items-start justify-start gap-4 w-full max-w-xl h-full text-balance sm:text-pretty text-center md:text-left">
             <span
               id="hero-badge"
@@ -49,7 +49,6 @@ const Homepage = () => {
                 Introducing NoteX — Notes, but better.
               </span>
             </span>
-
             <h1
               className="w-full text-4xl sm:text-7xl font-bold leading-none text-center md:text-left text-balance"
               style={{
@@ -58,29 +57,37 @@ const Homepage = () => {
             >
               Never throw away a thought
             </h1>
-            <p className="w-full md:text-xl text-zinc-500 text-center md:text-left text-balance">
+            <p className="hidden sm:block w-full md:text-xl text-zinc-500 text-center md:text-left text-balance">
               Effortlessly capture, organize, and bring your ideas to life. Your
               creativity, instantly at your fingertips.
             </p>
-
+            <p className="sm:hidden block w-full md:text-xl text-zinc-500 text-center md:text-left text-balance px-4">
+              Effortlessly capture, organize, and bring your ideas to life.
+            </p>
             <div
-              className="flex flex-col items-end justify-end gap-2 w-fit h-full"
+              className="flex flex-col md:flex-row items-center justify-start gap-2 w-full md:w-fit h-fit"
               id="hero-buttons"
             >
               <button
-                className="flex flex-col items-center justify-center gap-4 h-full px-6 py-3 my-2 text-pretty rounded-md text-zinc-50 bg-zinc-900 hover:bg-zinc-800 focus:bg-zinc-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-zinc-300 focus-visible:ring-opacity-50 focus-visible:ring-offset-zinc-900"
+                className="flex flex-col items-center justify-center gap-4 h-fit px-6 py-3 my-2 text-pretty rounded-md text-zinc-50 bg-zinc-900 hover:bg-zinc-800 focus:bg-zinc-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-zinc-300 focus-visible:ring-opacity-50 focus-visible:ring-offset-zinc-900"
                 id="start-creating-button"
               >
                 <span className="whitespace-nowrap">Start Creating</span>
               </button>
-            </div>
 
+              <span
+                className="flex items-center justify-center text-sm text-zinc-500 ml-2"
+                id="hero-pricing"
+              >
+                <span className="italic">It&apos;s free</span>, get started now!
+              </span>
+            </div>
             <ul
-              className="flex items-center justify-center md:justify-start gap-2 px-2 md:px-0 w-full md:w-fit md:max-w-md"
+              className="flex items-center justify-center md:justify-start px-2 md:px-0 gap-2 w-full md:w-fit md:max-w-md"
               id="hero-features"
             >
               <li
-                className="flex items-center justify-center gap-2 max-md:p-2 md:px-2 text-pretty rounded-md"
+                className="flex items-center justify-center gap-2 text-pretty rounded-md"
                 id="hero-feature"
               >
                 <Icon
@@ -103,6 +110,48 @@ const Homepage = () => {
                 <p>No registration</p>
               </li>
             </ul>
+          </div>
+
+          <div
+            className="flex flex-col items-center justify-start md:justify-center w-full md:w-fit h-fit mt-auto pt-2 md:pt-12"
+            id="hero-rating"
+          >
+            <div
+              className="flex flex-row items-center justify-center gap-1"
+              id="hero-stars"
+            >
+              <section
+                className="flex items-center justify-center gap-0.5 text-rose-500"
+                id="hero-stars"
+              >
+                {Array.from({ length: 5 }).map((_, i) => {
+                  return (
+                    <Icon
+                      key={i}
+                      name="Star"
+                      className={`w-4 h-4 text-rose-500 ${
+                        i < 4 ? 'fill-rose-500' : 'fill-transparent'
+                      }`}
+                      aria-hidden
+                    />
+                  );
+                })}
+              </section>
+
+              <span className="flex items-center justify-start gap-2 text-sm text-zinc-500 ml-2">
+                4.9 stars
+                <span className="sr-only">out of 5 stars</span>
+              </span>
+            </div>
+
+            <div
+              className="flex flex-col md:flex-row items-center justify-start md:justify-center gap-1 w-full"
+              id="hero-reviews"
+            >
+              <span className="w-fit md:w-full text-sm text-zinc-500 mt-2">
+                10,000+ reviews
+              </span>
+            </div>
           </div>
         </div>
 
