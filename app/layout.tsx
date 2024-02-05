cb8e9f9fb0f97f52f83e368702d3927124f5312a
gelzinn <60621494+gelzinn@users.noreply.github.com>
2024-02-05 16:42:39 -0300

    seo: add meta tags for SEO and social media sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,56 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en-US" className={`${archivo.variable} ${inter.variable}`}>
       <head>
         <title>Never throw away a thought | NoteX</title>
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="application-name" content="NoteX" />
+        <meta name="apple-mobile-web-app-title" content="NoteX" />
+
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-touch-fullscreen" content="yes" />
+
+        <meta name="description" content="Never throw away a thought | NoteX" />
+
+        <meta name="author" content="gelzin.com" />
+
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="bingbot" content="index, follow" />
+        <meta name="yandex" content="index, follow" />
+
+        <meta name="apple-mobile-web-app-title" content="NoteX" />
+        <meta name="application-name" content="NoteX" />
+
+        <meta name="theme-color" content="#ffffff" />
+
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-TileImage"
+          content="/favicons/favicon-light.png"
+        />
+
+        <meta property="og:site_name" content="NoteX" />
+        <meta
+          property="og:title"
+          content="Never throw away a thought | NoteX"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://notex.live" />
+        <meta
+          property="og:description"
+          content="Never throw away a thought | NoteX"
+        />
+
+        <meta property="og:image" content="/og/og-image.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="NoteX" />
+
+        <meta property="og:locale" content="en_US" />
 
         <link rel="icon" href="/favicons/favicon-light.png" type="image/png" />
       </head>
