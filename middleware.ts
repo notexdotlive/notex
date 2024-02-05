8b7a249776990923f971edc57acb8e839970de7d
gelzinn <mcnoobend@gmail.com>
2024-02-05 00:59:34 -0300

    fix: remove unnecessary code in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const url = new URL(request.url);
-  const origin = url.origin;
-  const pathname = url.pathname;
-  const headers = new Headers(request.headers);
-
-  headers.set('x-url', request.url);
-  headers.set('x-origin', origin);
-  headers.set('x-pathname', pathname);
-
   if (request.nextUrl.pathname === '/github')
     return NextResponse.redirect('https://github.com/gelzinn/notex');
-
-  return NextResponse.next({
-    request: {
-      headers,
-    },
-  });
 }
 
 export const config = {
