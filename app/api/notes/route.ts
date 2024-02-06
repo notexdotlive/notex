e92169878329952804f990fafc3c48beebcceb45
gelzinn <60621494+gelzinn@users.noreply.github.com>
2024-02-06 16:28:50 -0300

    fix: reorder api response on notes and temp remove filters

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -27,111 +27,33 @@ function extractUserText(content: ContentType | ContentType[]): string {
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-
   const params = Object.fromEntries(searchParams);
 
-  const search = params.q && params.q.trim();
-
-  const id = params.id && params.id.split(',').map((a) => a.trim());
-
-  const authors =
-    params.author && params.author.split(',').map((a) => +a.trim());
-  const statuses =
-    params.status && params.status.split(',').map((a) => a.trim());
-
   try {
-    if (search) {
-      const term = search.toLowerCase().trim();
+    const ref = collection(db, 'notes');
+    let q: any = query(ref);
 
-      const q = query(collection(db, 'notes'));
+    const snapshot = await getDocs(q);
+    const data = snapshot.docs.map((doc) => {
+      const id = doc.id;
+      const data: any = doc.data();
+      const orderedData: any = {};
 
-      const snapshot = await getDocs(q);
+      const keys = Object.keys(data).sort();
 
-      const data = snapshot.docs
-        .filter((doc) => {
-          const data = doc.data();
-          const { title, tags, content: cnt } = data;
+      keys.forEach((key) => {
+        orderedData[key] = data[key];
+      });
 
-          const content = JSON.parse(String(cnt)).content;
-          const note = extractUserText(content);
-
-          return (
-            title.toLowerCase().includes(term) ||
-            tags.some((tag: string) => tag.toLowerCase().includes(term)) ||
-            note.toLowerCase().includes(term)
-          );
-        })
-        .map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-          description: extractUserText(
-            JSON.parse(String(doc.data().content)).content,
-          ),
-        }));
-
-      return NextResponse.json(data, { status: 200 });
-    }
-
-    if (id) {
-      const q = query(collection(db, 'notes'), where('id', 'in', id));
-
-      const snapshot = await getDocs(q);
-
-      const data = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-
-      return NextResponse.json(data, { status: 200 });
-    }
-
-    if (authors) {
-      if (authors.some(isNaN))
-        return NextResponse.json(
-          { error: 'Invalid author ID' },
-          { status: 400 },
-        );
-
-      const q = query(
-        collection(db, 'notes'),
-        where('author_id', 'in', authors),
-      );
-
-      const snapshot = await getDocs(q);
-
-      const data = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-
-      return NextResponse.json(data, { status: 200 });
-    }
-
-    if (statuses) {
-      if (statuses.some((s) => !status.includes(s)))
-        return NextResponse.json({ error: 'Invalid status' }, { status: 400 });
-
-      const q = query(collection(db, 'notes'), where('status', 'in', statuses));
-
-      const snapshot = await getDocs(q);
-
-      const data = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-
-      return NextResponse.json(data, { status: 200 });
-    }
-
-    const snapshot = await getDocs(collection(db, 'notes'));
-
-    const data = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-      description: extractUserText(
-        JSON.parse(String(doc.data().content)).content,
-      ),
-    }));
+      return {
+        id,
+        ...orderedData,
+        content: data.content,
+        description: extractUserText(
+          JSON.parse(String(data.content)).content,
+        ).trim(),
+      };
+    });
 
     return NextResponse.json(data, { status: 200 });
   } catch (error: Error | any) {
