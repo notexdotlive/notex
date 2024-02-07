6e0d91c1bc7633054a0f59fc75460bf7bbafd812
gelzinn <60621494+gelzinn@users.noreply.github.com>
2024-02-07 16:00:40 -0300

    fix: update next imports

--- app/infra/next/client.ts
+++ app/infra/next/client.ts
@@ -1,6 +1,11 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import {
+  useSearchParams,
+  useRouter,
+  usePathname,
+  redirect,
+} from 'next/navigation';
 
-export { Link, Image, useSearchParams, useRouter, usePathname };
+export { Link, Image, useSearchParams, useRouter, usePathname, redirect };
