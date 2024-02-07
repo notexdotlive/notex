760e410ed91577a0370830362bc86ac272d9dad7
gelzinn <mcnoobend@gmail.com>
2024-02-06 23:37:32 -0300

    fix: update redirects in config file

--- app/config/redirects.ts
+++ app/config/redirects.ts
@@ -2,5 +2,7 @@ const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
 export const redirects = {
   '/new': `${baseUrl}/notes/new`,
-  '/github': 'https://github.com/gelzinn/notex',
+  '/github': 'https://github.com/notexdotlive/notex',
+  '/twitter': 'https://twitter.com/notexdotlive',
+  '/x': 'https://x.com/notexdotlive',
 };
